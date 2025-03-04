956440743eb51023af1459dd5601d9719df1e9ad
kiran <kirankalamata76@gmail.com>
2025-03-04 14:06:48 +0000

    post

--- myapp/views.py
+++ myapp/views.py
@@ -40,11 +40,11 @@ class StudentTotalData(APIView):
             std_class=Student.objects.all()
             serializer_class=StudentSerializer(std_class,many=True)
             return Response(serializer_class.data,status=status.HTTP_200_OK)
-    def post(self,request):
-        serializer_class=StudentSerializer(data=request.data)
+    def post(self, request):
+        serializer_class = StudentSerializer(data=request.data)
         if serializer_class.is_valid():
             serializer_class.save()
-            return Response(serializer_class.data,status=status.HTTP_201_CREATED)
+            return Response(serializer_class.data, status=status.HTTP_201_CREATED)
     def put(self,request,id):
         try:
             std_class=Student.objects.get(id=id)
@@ -63,4 +63,13 @@ class StudentTotalData(APIView):
                 raise Http404
             std_class.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
-  
+    def patch(self, request, id):
+        try:
+            std_class = Student.objects.get(id=id)
+        except Student.DoesNotExist:
+            raise Http404
+        serializer_class = StudentSerializer(std_class, data=request.data, partial=True)
+        if serializer_class.is_valid():
+            serializer_class.save()
+            return Response(serializer_class.data, status=status.HTTP_200_OK)
+        return Response(serializer_class.errors, status=status.HTTP_400_BAD_REQUEST)
